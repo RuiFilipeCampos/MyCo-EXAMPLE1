bed4596d655cfb375d034ba9bce0812fe791744b
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-06-20 13:14:44 +0100

    A

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 
-# Setting up path -> while installation is still not possible
-import sys
-sys.path.append(r"C:\Users\Rui Campos\Projects")
+
+## IF PYTHON PATH NOT SET UP
+## UNCOMMENT THESE TWO LINES AND REPLACE PATH BY MONTY CARLO PATH
+#import sys
+#sys.path.append(r"C:\Users\Rui Campos\Projects")
 
 from MontyCarlo import *
-
-
-
 water = Mat({1:2, 8:1}, 1, name = "Water")
 
 
